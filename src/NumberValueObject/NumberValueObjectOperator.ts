d68123d32f5479e126d4e84a4e6fe9daa51dc90f
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2023-09-07 17:32:59 -0500

    [Fix] NumberVOO

--- src/NumberValueObject/NumberValueObjectOperator.ts
+++ src/NumberValueObject/NumberValueObjectOperator.ts
@@ -2,34 +2,34 @@ import { IArithmeticOperator } from "../ValueObject/IArithmeticOperator";
 import { NumberValueObject } from "./NumberValueObject";
 
 export class NumberValueObjectOperator implements IArithmeticOperator<number> {
-  constructor(private _value: number) {}
+  constructor(private _value: NumberValueObject) {}
 
   plus(other: NumberValueObject): NumberValueObject {
-    return new NumberValueObject(this._value + other.valueOf());
+    return new NumberValueObject(this._value.valueOf() + other.valueOf());
   }
 
   substract(other: NumberValueObject): NumberValueObject {
-    return new NumberValueObject(this._value - other.valueOf());
+    return new NumberValueObject(this._value.valueOf() - other.valueOf());
   }
 
   times(x: number): NumberValueObject {
-    return new NumberValueObject(this._value * x.valueOf());
+    return new NumberValueObject(this._value.valueOf() * x.valueOf());
   }
 
   equalTo(other: number): boolean {
-    return this._value === other;
+    return this._value.valueOf() === other;
   }
 
   differsFrom(other: number): boolean {
-    return this._value !== other;
+    return this._value.valueOf() !== other;
   }
 
   isLessThan(other: number): boolean {
-    return this._value < other;
+    return this._value.valueOf() < other;
   }
 
   isBiggerThan(other: number): boolean {
-    return this._value > other;
+    return this._value.valueOf() > other;
   }
 
   isBiggerOrEqualThan(other: number): boolean {
