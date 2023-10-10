3bf693ad6144d473e375357ce77734bbeea50209
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2023-10-10 14:48:00 -0500

    [Fix] def

--- src/PasswordValueObject/errors/InvalidPasswordException.ts
+++ src/PasswordValueObject/errors/InvalidPasswordException.ts
@@ -1,12 +1,7 @@
-import { EmailValueObject } from "../../EmailValueObject";
-import { PasswordValueObject } from "../PasswordValueObject";
+import { StringValueObject } from "../../StringValueObject";
 
 export class InvalidPasswordException extends Error {
-  constructor(email: EmailValueObject, pass: PasswordValueObject) {
-    super(
-      `Invalid password: ${pass
-        .valueOf()
-        .toString()} for email: ${email.toString()}`
-    );
+  constructor(user: StringValueObject) {
+    super(`Invalid password for user: ${user}`);
   }
 }
